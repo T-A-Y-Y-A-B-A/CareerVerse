829f14fecf67f18c81440c020fe1d8419cf9585d
Tayyaba <tayyabaimtiaz554@gmail.com>
2026-06-17 21:50:15 +0500

    updates6

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   serverExternalPackages: ['pdf-parse', 'pdfjs-dist'],
   webpack: (config, { isServer }) => {
     if (!isServer) {
